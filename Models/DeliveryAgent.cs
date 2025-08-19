78950634c5ba5318a1fd9874e070b678924b047f
ancelia06 <114344319+ancelia06@users.noreply.github.com>
2025-08-19 19:08:58 +0530

    Delete Models directory

--- Models/DeliveryAgent.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FoodOrdering__API.Models
-{
-    public class DeliveryAgent
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
--- Models/FoodItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FoodOrdering__API.Models
-{
-    public class FoodItem
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-    }
-}
